30e0ebf00411b8be43f322a810d04399319d99e5
Mark Probst <mark.probst@gmail.com>
2018-04-10 14:55:33 -0700

    Don't include methods

--- typescript-json-schema.ts
+++ typescript-json-schema.ts
@@ -344,6 +344,10 @@ export class JsonSchemaGenerator {
     }
 
     private getDefinitionForProperty(prop: ts.Symbol, tc: ts.TypeChecker, node: ts.Node) {
+        if (prop.flags & ts.SymbolFlags.Method) {
+            return null;
+        }
+
         const propertyName = prop.getName();
         const propertyType = tc.getTypeOfSymbolAtLocation(prop, node);
 
@@ -627,7 +631,7 @@ export class JsonSchemaGenerator {
                 const requiredProps = props.reduce((required: string[], prop: ts.Symbol) => {
                     let def = {};
                     this.parseCommentsIntoDefinition(prop, def, {});
-                    if (!(prop.flags & ts.SymbolFlags.Optional) && !(<any>prop).mayBeUndefined && !def.hasOwnProperty("ignore")) {
+                    if (!(prop.flags & ts.SymbolFlags.Optional) && !(prop.flags & ts.SymbolFlags.Method) && !(<any>prop).mayBeUndefined && !def.hasOwnProperty("ignore")) {
                         required.push(prop.getName());
                     }
                     return required;
